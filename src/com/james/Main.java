f3d1c79ea76b3425dd00c80919efdf8325fde78b
James Mudidi <mudidi.jimmy@gmail.com>
2019-10-08 22:57:45 +0300

    initial commit

---
+++ src/com/james/Main.java
@@ -0,0 +1,16 @@
+package com.james;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MM d, yyyy'.'");
+        final LocalDateTime now = LocalDateTime.now();
+
+        System.out.println("Hello World");
+        System.out.println("The current time and date is " + dtf.format(now));
+    }
+
+}
